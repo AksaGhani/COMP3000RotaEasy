6b098738c04472291c8ab56f87ca78bcbfe3af8b
AksaGhani <57365302+AksaGhani@users.noreply.github.com>
2022-03-14 19:50:05 +0000

    Added frontend files for css, js and bootstrap libraries etc.

--- COMP3000RotaEasy/Startup.cs
+++ COMP3000RotaEasy/Startup.cs
@@ -48,12 +48,14 @@ namespace COMP3000RotaEasy
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
 
