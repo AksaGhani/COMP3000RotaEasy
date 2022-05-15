14dd00a34bb9f5cb658d173827438dd80b9dc899
AksaGhani <57365302+AksaGhani@users.noreply.github.com>
2022-05-15 21:40:58 +0100

    Added table to store shifts page

--- COMP3000RotaEasy/Pages/StoreShifts/Index.cshtml.cs
+++ COMP3000RotaEasy/Pages/StoreShifts/Index.cshtml.cs
@@ -5,9 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace COMP3000RotaEasy.Pages
+namespace COMP3000RotaEasy.Pages.StoreShifts
 {
-    public class SettingsModel : PageModel
+    public class IndexModel : PageModel
     {
         public void OnGet()
         {
