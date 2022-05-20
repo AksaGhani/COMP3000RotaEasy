a4d85d4b6a91cb437b899d53cece5a18c4b9da72
AksaGhani <57365302+AksaGhani@users.noreply.github.com>
2022-05-20 10:30:09 +0100

    Added Unit tests file structure

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using COMP3000RotaEasy.Models;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
